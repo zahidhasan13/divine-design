cd58195d434cdb741c0bac9c432a24b4bdbeaabb
zahidhasan13 <zahidhasanofficial13@gmail.com>
2022-08-27 21:40:41 +0600

    big change

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function openMenu(){
+    document.querySelector("#navmenu").style.right = '0px';
+}
+function closeMenu(){
+    document.querySelector("#navmenu").style.right = '-280px';
+}
--- js/script.js
+++
@@ -1,6 +0,0 @@
-function openMenu(){
-    document.getElementById("navmenu").style.right = '0px';
-}
-function closeMenu(){
-    document.getElementById("navmenu").style.right = '-280px';
-}
